325ee41869ce039296332894837265e79ab90a5f
Anastasiya <nast_danko_95@mail.ru>
2021-08-10 19:25:26 +0300

    feature: Add route, pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import AboutUs from './components/AboutUs';
+import Contacts from './components/Contacts';
 import Header from './components/Header';
+import Home from './components/Home';
 
 const App = () => (
   <>
     <Header />
+    <main>
+      <Switch>
+        <Route path='/about-us' component={AboutUs} />
+        <Route path='/contacts' component={Contacts} />
+        <Route path='/' component={Home}/>
+      </Switch>
+    </main>
   </>
 )
 
