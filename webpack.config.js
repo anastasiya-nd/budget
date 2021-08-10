e327a8b7185006c7519ba842df0e6ea3022872f7
Anastasiya <nast_danko_95@mail.ru>
2021-08-10 10:47:36 +0300

    feature: Add file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,25 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
+      },
+      {
+        test: /\.svg$/,
+        exclude: /(node_modules)/,
+        loader: 'file-loader',
       },
     ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: 'src/index.html',
+      template: './src/index.html',
     }),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   devServer: {
     open: true,
   },
