7b5b7b7b1f097e4aedbb1b0f29ec97375f32bb50
Anastasiya <nast_danko_95@mail.ru>
2021-08-11 22:01:14 +0300

    chore: Change Button props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ButtonItem from './styles';
 
-const Button = ({ text, variant}) => (
-  <ButtonItem className={variant} type="button" onClick={() => console.log('A method')}>
+const Button = ({ text, type, variant}) => (
+  <ButtonItem className={variant} type={type} onClick={() => console.log('A method')}>
     {text}
   </ButtonItem>
 )
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  type: PropTypes.string,
   variant: PropTypes.string
 };
 
 Button.defaultProps = {
+  type: 'button',
   variant: 'primary',
 };
 
