2324decf3a866e2310d6197b3b9792d664fd6b2b
Anastasiya <nast_danko_95@mail.ru>
2021-08-16 17:21:42 +0300

    feature: Add styled-normalize, fonts

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,48 @@
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+import regularFont from './assets/fonts/OpenSans-Regular.ttf';
+import lightFont from './assets/fonts/OpenSans-Light.ttf';
+import boldFont from './assets/fonts/OpenSans-Bold.ttf';
 
-const Main = styled.main`
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  @font-face {
+    font-family: 'OpenSans';
+    src: url(${regularFont}) format('truetype');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'OpenSans';
+    src: url(${lightFont}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'OpenSans';
+    src: url(${boldFont}) format('truetype');
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  body {
+    font-family: 'OpenSans';
+    font-weight: 400;
+  }
+
+  ul {
+    padding: 0;
+  }
+
+  ul li {
+    list-style-type: none;
+  }
+`;
+
+export const Main = styled.main`
   max-width: 980px;
   margin: 0 auto;
 `;
-
-export default Main;
