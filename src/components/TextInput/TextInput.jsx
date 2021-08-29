4977eb6cd5dd533e3423645a07299604071618cc
Anastasiya <nast_danko_95@mail.ru>
2021-08-29 03:38:04 +0300

    feature: Add state for amount

--- src/components/TextInput/TextInput.jsx
+++ src/components/TextInput/TextInput.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Input, Label } from './styles';
 
-const TextInput = ({ label, value }) => {
+const TextInput = ({ label }) => {
+  const [currentValue, setValue] = useState(0);
   return (
-    <div>
+    <>
       {label && <Label htmlFor="amount">{label}</Label>}
-      <Input id="amount" type="number" name="amount" value={value} />
-    </div>
+      <Input
+        id="amount"
+        type="number"
+        name="amount"
+        value={currentValue}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </>
   );
 };
 
 TextInput.propTypes = {
   label: PropTypes.string,
-  value: PropTypes.number,
 };
 
 TextInput.defaultProps = {
   label: '',
-  value: 0,
 };
 
 export default TextInput;
