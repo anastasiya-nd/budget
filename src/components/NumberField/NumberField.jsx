ded81a740065a62c51a4e5929246e99da5c95954
Anastasiya <nast_danko_95@mail.ru>
2021-09-11 11:10:59 +0300

    feature: Create NumberField

---
+++ src/components/NumberField/NumberField.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Input, Label } from './styles';
+
+const NumberField = ({ label, value, onChange }) => {
+  const [inputValue, setInputValue] = useState(value);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const validateInputValue = () => {
+    let val = parseFloat(inputValue);
+    if (isNaN(val)) { // eslint-disable-line
+      val = '';
+      setInputValue(val);
+    } else {
+      setInputValue(val);
+      onChange(val);
+    }
+  };
+
+  return (
+    <div>
+      {label && <Label htmlFor="amount">{label}</Label>}
+      <Input
+        id="amount"
+        type="text"
+        name="amount"
+        value={inputValue}
+        onChange={handleInputChange}
+        onBlur={validateInputValue}
+      />
+    </div>
+  );
+};
+
+NumberField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+NumberField.defaultProps = {
+  label: '',
+};
+
+export default NumberField;
