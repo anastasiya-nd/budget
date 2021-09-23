8b021726bac49827a7e8d7cb2fdef793652fb296
Anastasiya <nast_danko_95@mail.ru>
2021-09-23 12:08:44 +0300

    chore: Change store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
 import { applyMiddleware, createStore } from 'redux';
-import createSagaMiddleware from '@redux-saga/core';
+import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
 import rootSaga from './sagas';
 
 const saga = createSagaMiddleware();
-const store = createStore(reducer, applyMiddleware(saga));
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducer,
+  applyMiddleware(saga),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 saga.run(rootSaga);
 
