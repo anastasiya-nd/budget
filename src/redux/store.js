7ea281c7df6d0dcb183641be9c90fdb53b1b6224
Anastasiya <nast_danko_95@mail.ru>
2021-09-23 12:18:29 +0300

    feature: Add redux-devtools-extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducer';
 import rootSaga from './sagas';
 
 const saga = createSagaMiddleware();
 
-/* eslint-disable no-underscore-dangle */
-const store = createStore(
-  reducer,
-  applyMiddleware(saga),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-/* eslint-enable */
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(saga)));
 
 saga.run(rootSaga);
 
