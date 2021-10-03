2afcd1660ba31bae501e74b1edc0954c471e39a3
Anastasiya <nast_danko_95@mail.ru>
2021-10-03 20:31:36 +0300

    feature: Create Pagination

---
+++ src/components/Pagination/Pagination.jsx
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  PaginationWrap,
+  PaginationButtonsWrap,
+  PaginationItem,
+  PaginationDots,
+  PaginationButton,
+  PaginationNavigation,
+} from './styles';
+import PrevArrow24 from '../Icons/PrevArrow24';
+import NextArrow24 from '../Icons/NextArrow24';
+
+const Pagination = ({ currentPage, total, onPageChange }) => {
+  const [buttons, setButtons] = useState([]);
+
+  const numberOfPages = [];
+  for (let i = 1; i <= total; i++) { //eslint-disable-line
+    numberOfPages.push(i);
+  }
+
+  useEffect(() => {
+    let paginationArray;
+    const leftDots = '...';
+    const rightDots = ' ...';
+
+    if (total <= 9) {
+      paginationArray = numberOfPages;
+    } else if (currentPage < 5) {
+      const sliced1 = numberOfPages.slice(0, 6);
+      const sliced2 = numberOfPages.slice(-3);
+      paginationArray = [...sliced1, leftDots, ...sliced2];
+    } else if (currentPage >= 5) {
+      const sliced1 = numberOfPages.slice(0, 3);
+      const sliced2 = numberOfPages.slice(-3);
+      const sliced3 = numberOfPages.slice(`${currentPage - 3}`, `${currentPage + 2}`);
+      if (
+        sliced3[0] - sliced1[sliced1.length - 1] > 1 &&
+        sliced2[0] - sliced3[sliced3.length - 1] > 1
+      ) {
+        paginationArray = [...sliced1, leftDots, ...sliced3, rightDots, ...sliced2];
+      } else if (sliced1[sliced1.length - 1] - sliced3[0] > 1) {
+        paginationArray = [...sliced1, leftDots, ...sliced3, ...sliced2];
+      } else if (sliced2[0] - sliced3[sliced3.length - 1] > 1) {
+        paginationArray = [...sliced1, ...sliced3, rightDots, ...sliced2];
+      } else {
+        paginationArray = [...sliced1, leftDots, ...sliced3, ...sliced2];
+      }
+    }
+
+    paginationArray = Array.from(new Set(paginationArray));
+    setButtons(paginationArray);
+  }, [currentPage, total]);
+
+  return (
+    <PaginationWrap>
+      <PaginationNavigation
+        type="button"
+        onClick={() => onPageChange(currentPage - 1)}
+        disabled={currentPage === 1}
+      >
+        <PrevArrow24 />
+        Prev
+      </PaginationNavigation>
+      <PaginationButtonsWrap>
+        {buttons.map((button) => {
+          return (
+            <PaginationItem key={button}>
+              {typeof button === 'number' ? (
+                <PaginationButton
+                  type="button"
+                  onClick={() => onPageChange(button)}
+                  active={+button === +currentPage}
+                >
+                  {button}
+                </PaginationButton>
+              ) : (
+                <PaginationDots>{button}</PaginationDots>
+              )}
+            </PaginationItem>
+          );
+        })}
+      </PaginationButtonsWrap>
+      <PaginationNavigation
+        type="button"
+        onClick={() => onPageChange(currentPage + 1)}
+        disabled={currentPage === total}
+      >
+        Next
+        <NextArrow24 />
+      </PaginationNavigation>
+    </PaginationWrap>
+  );
+};
+
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequired,
+  total: PropTypes.number,
+  onPageChange: PropTypes.func.isRequired,
+  className: PropTypes.string, //eslint-disable-line
+};
+
+Pagination.defaultProps = {
+  total: 0,
+};
+
+export default Pagination;
---
+++ src/components/Pagination/styles.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+
+const commonStylesOfPagintaionItem = `
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 16px;
+  padding: 0 2px;
+`;
+
+export const PaginationWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const PaginationButtonsWrap = styled.ul`
+  display: flex;
+  align-items: center;
+  margin: 0 24px;
+`;
+
+export const PaginationItem = styled.li`
+  margin: 0 4px;
+`;
+
+export const PaginationButton = styled.button`
+  color: ${({ active }) => (active ? '#24695C' : '#b2b2b2')};
+  transition: 0.15s;
+  ${commonStylesOfPagintaionItem}
+  &:hover {
+    color: #24695c;
+    cursor: pointer;
+  }
+`;
+
+export const PaginationDots = styled.span`
+  ${commonStylesOfPagintaionItem}
+  color: #b2b2b2;
+`;
+
+export const PaginationNavigation = styled.button`
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  line-height: 14px;
+  color: #b2b2b2;
+  transition: 0.15s;
+  font-weight: 600;
+  svg {
+    fill: #b2b2b2;
+    transition: 0.15s;
+  }
+  &:hover {
+    color: #24695c;
+    cursor: pointer;
+  }
+  &:hover svg {
+    fill: #24695c;
+  }
+  &:disabled {
+    color: #cbcbcb;
+  }
+  &:disabled svg {
+    fill: #cbcbcb;
+  }
+`;
